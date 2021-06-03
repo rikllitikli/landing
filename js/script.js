20e38d729b36ae23a07c0f41b59a8b1027e87add
Kirill Pushkarow <kirrrilll98@gmail.com>
2021-06-03 12:16:18 +0300

    add

--- js/script.js
+++ js/script.js
@@ -1,20 +1,16 @@
-$(document).ready(function(){
-
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:0,
-        nav:true,
-        navText: [ '', ' ' ],
-
-        responsive:{
-            0:{
-                items:1
-            },
-
-            1000:{
-                items:3
-            }
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
         }
-    });
-});
-
+    }
+})
